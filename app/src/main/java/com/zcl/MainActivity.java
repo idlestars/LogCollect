48962991ff582524214a76c059e992dc792bbacf
everhad <1294828431@qq.com>
2016-07-09 14:34:53 +0800

    【Modify】 MainActivity代码优化。

--- app/src/main/java/com/zcl/MainActivity.java
+++ app/src/main/java/com/zcl/MainActivity.java
@@ -8,20 +8,18 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Gallery;
-import android.widget.ImageView;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends Activity implements OnClickListener {
 	private Fragment[] mFragments;
 	private FragmentManager fragmentManager;
-	private FragmentTransaction fragmentTransaction;
 	private Gallery gallery;
+	private View selectedMenu;
+	private int selectedFragment = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +27,10 @@ public class MainActivity extends Activity implements OnClickListener {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
+		View home = findViewById(R.id.iv_shouye);
 		findViewById(R.id.iv_mylog).setOnClickListener(this);
 		findViewById(R.id.iv_myself).setOnClickListener(this);
-		findViewById(R.id.iv_shouye).setOnClickListener(this);
+		home.setOnClickListener(this);
 		findViewById(R.id.iv_meg).setOnClickListener(this);
 
 		gallery = (Gallery) findViewById(R.id.gallery1);
@@ -43,58 +42,44 @@ public class MainActivity extends Activity implements OnClickListener {
 		mFragments[1] = fragmentManager.findFragmentById(R.id.fragement_mylog);
 		mFragments[2] = fragmentManager.findFragmentById(R.id.fragement_meg);
 		mFragments[3] = fragmentManager.findFragmentById(R.id.fragement_myself);
-		fragmentTransaction = fragmentManager.beginTransaction()
-				.hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2])
-				.hide(mFragments[3]);
-		fragmentTransaction.show(mFragments[0]).commit();
+
+		hideAllFragments();
+		onClick(home);
 	}
 
-	private View prevSelectedMenu;
 	@Override
 	public void onClick(View v) {
-		if (prevSelectedMenu != null ) {
-			prevSelectedMenu.setSelected(false);
+		if (v == selectedMenu) return;
+		if (selectedMenu != null) {
+			selectedMenu.setSelected(false);
 		}
 		v.setSelected(true);
-		prevSelectedMenu = v;
+		selectedMenu = v;
 
-		fragmentTransaction = fragmentManager.beginTransaction()
-				.hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2])
-				.hide(mFragments[3]);
+		FragmentTransaction transaction = fragmentManager.beginTransaction();
+		transaction.hide(mFragments[selectedFragment]);
+
 		switch (v.getId()) {
-		case R.id.iv_shouye:
-
-			fragmentTransaction.show(mFragments[0]).commit();
-			break;
-		case R.id.iv_mylog:
-			fragmentTransaction.show(mFragments[1]).commit();
-			break;
-		case R.id.iv_meg:
-			fragmentTransaction.show(mFragments[2]).commit();
-			break;
-		case R.id.iv_myself:
-			fragmentTransaction.show(mFragments[3]).commit();
-			break;
+			case R.id.iv_shouye:
+				selectedFragment = 0;
+				break;
+			case R.id.iv_mylog:
+				selectedFragment = 1;
+				break;
+			case R.id.iv_meg:
+				selectedFragment = 2;
+				break;
+			case R.id.iv_myself:
+				selectedFragment = 3;
+				break;
 		}
+
+		transaction.show(mFragments[selectedFragment]).commit();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void hideAllFragments() {
+		fragmentManager.beginTransaction()
+				.hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2])
+				.hide(mFragments[3]).commit();
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }
